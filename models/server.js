e1821a8e4d9c8a2f6b29441d5c4e00f0a894942e
MPulgarA <matiaspulgaralarcon@gmail.com>
2021-09-28 10:56:13 -0300

    Configuración del model server

--- models/server.js
+++ models/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const db = require('./../db/connection');
 
 
 class Server{
@@ -7,21 +9,33 @@ class Server{
         this.port = process.env.PORT;
 
         this.paths = {
-
+            products: '/products',
         }
 
-
+        this.dbConnection();
         this.middlewares();
         this.routes();
         
     }
 
     middlewares(){
-
+        this.app.use(cors());
+        this.app.use(express.json());
     }
 
     routes(){
-        
+        this.app.use(this.paths.products, require('./../routes/products')); 
+    }
+
+    async dbConnection(){
+        try {
+            // await db.authenticate();
+            await db.sync();
+            console.log('La base de datos esta online');
+        } catch (error) {
+            console.log('La conexión no se ha podido realizar');
+            throw new Error(error);
+        }
     }
 
     listen(){
