ee313df12a31b8776c520616ae941007ef6c390b
MPulgarA <matiaspulgaralarcon@gmail.com>
2021-10-07 12:11:40 -0300

    Creacion rutas producto

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,57 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+
+const { listAllProducts,
+        listProductById,
+        createProduct,
+        updateProductById,
+        deleteProductById } = require('./../controllers/products');
+
+const { existProductById } = require('./../helpers/db-validator');
+const { existProduct } = require('./../helpers/product-validator');
+const {validateFields} = require('./../middlewares/validateFields');
+
 
 
 const router = Router();
 
-router.get('/', );
-router.get('/:id', );
-router.post('/', );
-router.put('/:id', );
-router.delete('/:id', );
+// TODO Incorporación de helpers y express-validator
 
+
+
+router.get('/', listAllProducts);
+
+router.get('/:id',[
+    check('id').custom(existProductById),
+    validateFields
+], listProductById);
+
+// router.get('/hello', (req, res) =>{
+//     res.send('Hola');
+// });
+
+router.post('/',[
+    check('name').custom(existProduct),
+    check('name', 'El nombre del producto es obligatorio').not().isEmpty(),
+    check('description', 'La descripción del producto es obligatorio').not().isEmpty(),
+    check('price', 'El precio del producto es obligatorio').not().isEmpty(),
+    validateFields
+], createProduct);
+
+router.put('/:id',[
+    check('id').custom(existProductById),
+    check('name').custom(existProduct),
+    check('name', 'El nombre del producto es obligatorio').not().isEmpty(),
+    check('description', 'La descripción del producto es obligatorio').not().isEmpty(),
+    check('price', 'El precio del producto es obligatorio').not().isEmpty(),
+    validateFields
+], updateProductById);
+
+router.delete('/:id',[
+    check('id').custom(existProductById),
+    validateFields
+], deleteProductById);
 
 
 
